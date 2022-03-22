328c348b414b4a118eee21abb23982cfd05bf09d
Adam Phi Vo <adamphivo@gmail.com>
2022-03-22 13:20:00 +0100

    Add validation to Scan Form

--- src/lib/components/modules/Node/_NodeDataForm.svelte
+++ src/lib/components/modules/Node/_NodeDataForm.svelte
@@ -2,19 +2,24 @@
     import Button from "$lib/components/modules/Elements/Button.svelte";
     import { scannedNodeData } from "$lib/stores/scannedNodeData";
     import { send } from "$lib/helpers/send";
+    import { validate } from "$lib/helpers/validate";
 
     let address = $scannedNodeData?.address || "";
 
     async function scan() {
         if (address) {
-            const response = await send(
-                "GET",
-                `brubeck/node/${address.toLowerCase()}.json`
-            );
-            const node = await response.json();
+            if (validate.ethAddress(address)) {
+                const response = await send(
+                    "GET",
+                    `brubeck/node/${address.toLowerCase()}.json`
+                );
+                const node = await response.json();
 
-            if (node) {
-                $scannedNodeData = node;
+                if (node) {
+                    $scannedNodeData = node;
+                }
+            } else {
+                throw new Error();
             }
         } else {
             throw new Error();
@@ -24,10 +29,7 @@
 
 <div class="module">
     <div class="scanner">
-        <input
-            bind:value={address}
-            placeholder="ETH Public Key"
-        />
+        <input bind:value={address} placeholder="ETH Public Key" />
         <Button action={scan} text="Scan" />
     </div>
 </div>
---
+++ src/lib/helpers/validate.ts
@@ -0,0 +1,8 @@
+const validate = () => { };
+
+validate.ethAddress = (address: string): boolean => {
+    const regex = /0x[a-fA-F0-9]{40}/g;
+    return regex.test(address);
+}
+
+export { validate };
