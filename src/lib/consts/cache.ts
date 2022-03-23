a23862ee84f26fd0cb9061636aacc9b33914db53
Adam Phi Vo <adamphivo@gmail.com>
2022-03-23 23:49:00 +0100

    Reduce Cache TTL

--- src/lib/consts/cache.ts
+++ src/lib/consts/cache.ts
@@ -1,5 +1,5 @@
 export const cache = {
-    GLOBAL_TTL: 120,
+    GLOBAL_TTL: 10,
     BRUBECK_STATS: "brubeckStats",
     MARKET_PRICES: "marketPrices",
     NODE_AGGREGATED_DATA_BASE: "nodeAggregatedData_",
