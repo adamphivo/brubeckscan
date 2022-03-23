0e4b0faf86c611e519124878eee35473e28f4687
Adam Phi Vo <adamphivo@gmail.com>
2022-03-23 01:17:05 +0100

    Fix caching bug

--- src/routes/api/market/prices/index.json.ts
+++ src/routes/api/market/prices/index.json.ts
@@ -1,39 +1,40 @@
 import { appCache } from "$lib/helpers/cache";
 
 export async function get() {
-    const cached = appCache.get("marketPrices");
+    const cached = appCache.get("marketPrices")
 
     if (cached) {
         return {
-            status: 304,
             body: cached
         }
     } else {
         const BINANCE_BASE =
             "https://api.binance.com/api/v3/ticker/price?symbol=";
-
+    
         const symbols = ["BTCUSDT", "DATAUSDT"];
-
+    
         const requests = symbols.map(async (symbol) => {
             const request = await fetch(`${BINANCE_BASE}${symbol}`)
             return request;
         });
-
+    
         const responses = await Promise.all(requests);
-
+    
         const data = await Promise.all(responses.map((res) => res.json()));
-
+    
         let prices: any = {}
-
+    
         data.forEach(price => {
             prices[price.symbol] = price.price
         })
-
-        appCache.set('marketPrices', prices, 60);
-
+    
+        const save = appCache.set("marketPrices", prices, 60);
+    
         return {
             status: 200,
             body: prices,
+            maxage: 60
         };
     }
+
 }
