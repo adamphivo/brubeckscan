bbc6a5f113150d41f2ac857db6826b5117c5b6aa
Adam Phi Vo <adamphivo@gmail.com>
2022-03-26 18:42:53 +0100

    Hotfix on watchlistpage

--- src/routes/watchlist/index.svelte
+++ src/routes/watchlist/index.svelte
@@ -1,9 +1,24 @@
+<script context="module" lang="ts">
+    import { get } from "svelte/store";
+    import { userData } from "$lib/stores/userData";
+
+    export async function load({ request, params }) {
+        if (!get(userData)) {
+            return {
+                status: 301,
+                redirect: "/",
+            };
+        }
+        return {};
+    }
+</script>
+
 <script lang="ts">
     import Graph from "$lib/components/Watchlist/_Graph.svelte";
     import Watchlist from "$lib/components/Watchlist/Watchlist.svelte";
     import PageTitle from "$lib/components/Layout/PageTitle.svelte";
     import Summary from "$lib/components/Watchlist/_Summary.svelte";
-    
+
     const TITLE = "Watchlist";
 </script>
 
