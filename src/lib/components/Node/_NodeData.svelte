f09ecfbdf593f3fd7918e4b783881bb4c23d03d7
Adam Phi Vo <adamphivo@gmail.com>
2022-04-10 23:14:25 +0200

    Improve individual node status

--- src/lib/components/Node/_NodeData.svelte
+++ src/lib/components/Node/_NodeData.svelte
@@ -1,169 +1,201 @@
 <script lang="ts">
-    import Format from "$lib/helpers/format";
-    import { page } from "$app/stores";
-    import { onMount } from "svelte";
-    import { scannedNodeData } from "$lib/stores/scannedNodeData";
-    import { marketPrices } from "$lib/stores/marketPrices";
-    import { userData } from "$lib/stores/userData";
-    import Bookmark from "./_Bookmark.svelte";
-    import { send } from "$lib/helpers/send";
-    import { validate } from "$lib/helpers/validate";
+  import Format from "$lib/helpers/format";
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
+  import { scannedNodeData } from "$lib/stores/scannedNodeData";
+  import { marketPrices } from "$lib/stores/marketPrices";
+  import { userData } from "$lib/stores/userData";
+  import Bookmark from "./_Bookmark.svelte";
+  import { send } from "$lib/helpers/send";
+  import { validate } from "$lib/helpers/validate";
+  import { selectedScanCurrency } from "$lib/stores/selectedCurrency";
+  import CurrencySelector from "./_CurrencySelector.svelte";
 
-    $: isUserOwner = $userData?.address === $scannedNodeData?.address;
+  $: isUserOwner = $userData?.address === $scannedNodeData?.address;
 
-    let copyText = "Copy";
+  let copyText = "Copy";
 
-    function copyAddressToClipboard() {
-        navigator.clipboard.writeText($scannedNodeData.address);
-        copyText = "Copied";
-        setTimeout(() => (copyText = "Copy"), 2000);
-    }
+  function copyAddressToClipboard() {
+    navigator.clipboard.writeText($scannedNodeData.address);
+    copyText = "Copied";
+    setTimeout(() => (copyText = "Copy"), 2000);
+  }
 
-    onMount(async () => {
-        const address = $page.url.searchParams.get('address');
-        if (address) {
-            if (validate.ethAddress(address)) {
-                const response = await send(
-                    "GET",
-                    `brubeck/node/${address.toLowerCase()}.json`
-                );
-                const node = await response.json();
+  onMount(async () => {
+    const address = $page.url.searchParams.get("address");
+    if (address) {
+      if (validate.ethAddress(address)) {
+        const response = await send(
+          "GET",
+          `brubeck/node/${address.toLowerCase()}.json`
+        );
+        const node = await response.json();
 
-                if (node) {
-                    $scannedNodeData = node;
-                }
-            } else {
-                throw new Error();
-            }
-        } else {
-            return {};
+        if (node) {
+          $scannedNodeData = node;
         }
-    })
+      } else {
+        throw new Error();
+      }
+    } else {
+      return {};
+    }
+  });
 </script>
 
 {#if $scannedNodeData}
-    <div class="module {isUserOwner ? 'owner' : ''}">
-        {#if $userData}
-            <div class="data">
-                {#if isUserOwner}
-                    <div
-                        title="Owner"
-                        style:background={$userData.profile.gradient}
-                        class="userGradient"
-                    />
-                {/if}
-                <Bookmark />
-            </div>
+  <div class="module {isUserOwner ? 'owner' : ''}">
+    {#if $userData}
+      <div class="data">
+        {#if isUserOwner}
+          <div
+            title="Owner"
+            style:background={$userData.profile.gradient}
+            class="userGradient"
+          />
         {/if}
-        <div />
-        <div class="data">
-            <div class="label">
-                <p>ETH Node Public Address</p>
-            </div>
-            <div class="value">
-                <p>
-                    {Format.shortenNodeAddress($scannedNodeData.address)}
-                </p>
-                <button on:click={copyAddressToClipboard}>{copyText}</button>
-            </div>
+        <Bookmark />
+      </div>
+    {/if}
+    <div />
+    <div class="data">
+        <div class="label">
+            <p>Node ETH Public Address</p>
         </div>
-        <div class="separator"></div>
-        <div class="data">
-            <div class="label">
-                <p>Node Rewards in DATA</p>
-            </div>
-            <div class="value">
-                <p>{Format.tokenValue($scannedNodeData.rewardsInData)}</p>
-                <p>DATA</p>
-            </div>
-        </div>
-        <div class="separator"></div>
-        <div class="data">
-            <div class="label">
-                <p>Node Rewards in USDT</p>
-            </div>
-            <div class="value">
-                <p>
-                    {Format.tokenValue(
-                        $scannedNodeData.rewardsInData * $marketPrices.DATAUSDT
-                    )}
-                </p>
-                <p>USDT</p>
-            </div>
-        </div>
-        <div class="separator"></div>
-        <div class="data">
-            <div class="label">
-                <p>Received DATA</p>
-            </div>
-            <div class="value">
-                <p>{Format.tokenValue($scannedNodeData.dataSent)}</p>
-                <p>DATA</p>
-            </div>
-        </div>
-        <div class="separator"></div>
-        <div class="data">
-            <div class="label">
-                <p>DATA to be received</p>
-            </div>
-            <div class="value">
-                <p>{Format.tokenValue($scannedNodeData.rewardsInData - $scannedNodeData.dataSent)}</p>
-                <p>DATA</p>
-            </div>
-        </div>
-        <div class="separator"></div>
-        <div class="data">
-            <div class="label">
-                <p>Staked DATA</p>
-            </div>
-            <div class="value">
-                <p>{Format.tokenValue($scannedNodeData.dataStaked)}</p>
-                <p>DATA</p>
-            </div>
-        </div>
-        <div class="separator"></div>
-        <div class="data">
-            <div class="label">
-                <p>Staked value in USDT</p>
-            </div>
-            <div class="value">
-                <p>
-                    {Format.tokenValue(
-                        $scannedNodeData.dataStaked * $marketPrices.DATAUSDT
-                    )}
-                </p>
-                <p>USDT</p>
-            </div>
-        </div>
-        <div class="separator"></div>
-        <div class="data">
-            <div class="label">
-                <p>Claim Count</p>
-            </div>
-            <div class="value">
-                <p>{$scannedNodeData.claimCount}</p>
-            </div>
+        <div class="value">
+            <p>
+                {Format.shortenNodeAddress($scannedNodeData.address)}
+            </p>
+            <button on:click={copyAddressToClipboard}>{copyText}</button>
         </div>
     </div>
+    <div class="separator" />
+    <CurrencySelector />
+    <div class="separator" />
+    <div class="data">
+      <div class="label">
+        <p>Staked</p>
+      </div>
+      <div class="value">
+        {#if $selectedScanCurrency === "data"}
+          <p>{Format.tokenValue($scannedNodeData.dataStaked)}</p>
+          <p class="currency {$selectedScanCurrency}">DATA</p>
+        {:else}
+          <p>
+            {Format.tokenValue(
+              $scannedNodeData.dataStaked * $marketPrices.DATAUSDT
+            )}
+          </p>
+          <p class="currency {$selectedScanCurrency}">USDT</p>
+        {/if}
+      </div>
+    </div>
+    <div class="separator" />
+    <div class="data">
+      <div class="label">
+        <p>Accumulated rewards</p>
+      </div>
+      <div class="value {$selectedScanCurrency}">
+        {#if $selectedScanCurrency === "data"}
+          <p>{Format.twoDecimals($scannedNodeData.rewardsInData)}</p>
+          <p class="currency {$selectedScanCurrency}">DATA</p>
+        {:else}
+          <p>
+            {Format.tokenValue(
+              $scannedNodeData.rewardsInData * $marketPrices.DATAUSDT
+            )}
+          </p>
+          <p class="currency {$selectedScanCurrency}">USDT</p>
+        {/if}
+      </div>
+    </div>
+    <div class="separator" />
+    <div class="data">
+      <div class="label">
+        <p>Received rewards</p>
+      </div>
+      <div class="value {$selectedScanCurrency}">
+        {#if $selectedScanCurrency === "data"}
+          <p>{Format.twoDecimals($scannedNodeData.dataSent)}</p>
+          <p class="currency {$selectedScanCurrency}">DATA</p>
+        {:else}
+          <p>
+            {Format.twoDecimals(
+              $scannedNodeData.dataSent * $marketPrices["DATAUSDT"]
+            )}
+          </p>
+          <p class="currency {$selectedScanCurrency}">USDT</p>
+        {/if}
+      </div>
+    </div>
+    <div class="separator" />
+    <div class="data">
+      <div class="label">
+        <p>Rewards to be received</p>
+      </div>
+      <div class="value {$selectedScanCurrency}">
+        {#if $selectedScanCurrency === "data"}
+          <p>
+            {Format.twoDecimals(
+              $scannedNodeData.rewardsInData -
+                $scannedNodeData.dataSent
+            )}
+          </p>
+          <p class="currency {$selectedScanCurrency}">DATA</p>
+        {:else}
+          <p>
+            {Format.twoDecimals(
+              ($scannedNodeData.rewardsInData -
+                $scannedNodeData.dataSent) *
+                $marketPrices["DATAUSDT"]
+            )}
+          </p>
+          <p class="currency {$selectedScanCurrency}">USDT</p>
+        {/if}
+      </div>
+    </div>
+    <div class="separator" />
+    <div class="data">
+      <div class="label">
+        <p>Claim count</p>
+      </div>
+      <div class="value">
+        <p>
+          {$scannedNodeData.claimCount} reward code{$scannedNodeData.claimCount >
+          1
+            ? "s"
+            : ""}
+        </p>
+      </div>
+    </div>
+  </div>
 {/if}
 
 <style lang="scss">
-    .module {
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
-        border: thin solid transparent;
-        border-radius: 0;
-    }
-    .userGradient {
-        width: 30px;
-        height: 30px;
-        border-radius: 50%;
-    }
+  .module {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    border: thin solid transparent;
+    border-radius: 0;
+  }
+  .userGradient {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+  }
 
-    .module.owner {
-        border: thin solid rgb(67, 233, 205);
-        border-radius: 0;
-    }
+  .module.owner {
+    border: thin solid rgb(67, 233, 205);
+    border-radius: 0;
+  }
+
+  .currency.data {
+    color: var(--color-orange);
+  }
+
+  .currency.usdt {
+    color: rgb(100, 218, 161);
+  }
 </style>
--- src/lib/components/Watchlist/List/Table/Cells/_Status.svelte
+++ src/lib/components/Watchlist/List/Table/Cells/_Status.svelte
@@ -1,67 +1,62 @@
 <script lang="ts">
-    import { brubeckData } from "$lib/stores/brubeckData";
-    import { time } from "$lib/helpers/time";
-
-    export let node;
+  import { formatDistance } from "date-fns";
+  import { brubeckData } from "$lib/stores/brubeckData";
+  export let node;
+  const now = new Date();
+  const latestClaim = new Date(node?.claimedRewardCodes[0]?.claimTime);
+  const distance = formatDistance(latestClaim, now, { addSuffix: true });
+  const hasClaimedTheLatestKnownCode = node?.claimedRewardCodes.find(
+    (code) => code.id === $brubeckData.lastCode
+  );
 </script>
 
 <td>
-    <div class="container">
-        {#if node?.claimedRewardCodes.length === 0}
-            <p class="ko" title="This node never claimed a reward code">KO</p>
-        {:else}
-            {#if node?.claimedRewardCodes[0]?.id === $brubeckData?.lastCode}
-                <p class="ok" title="Claimed the latest reward code">OK</p>
-            {:else}
-                <p class="yellow" title="Reward code distribution ongoing">
-                    ~
-                </p>
-            {/if}
-            <div title="Time since last reward code was claimed by that node">
-                {time.since(
-                    new Date(node?.claimedRewardCodes[0]?.claimTime),
-                    "minutes"
-                )}
-            </div>
-        {/if}
-    </div>
+  <div class="container">
+    {#if node?.claimedRewardCodes.length === 0}
+      <p class="ko" title="This node never claimed a reward code">KO</p>
+    {:else}
+      {#if hasClaimedTheLatestKnownCode}
+        <p class="ok" title="This node claimed the latest reward code">OK</p>
+      {:else}
+        <p class="ko" title="This node did not claimed the latest reward code">KO</p>
+      {/if}
+      <p title="Last reward code claimed">{distance}</p>
+    {/if}
+  </div>
 </td>
 
 <style lang="scss">
-    td {
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        position: relative;
-        width: 150px;
-    }
+  td {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    position: relative;
+    width: 150px;
+  }
 
-    p {
-        font-weight: bold;
-    }
+  .container {
+    font-size: 12px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+  }
 
-    .ok {
-        color: lightgreen;
-    }
-    .ko {
-        color: rgb(226, 88, 88);
-    }
+  p {
+    font-weight: bold;
+  }
 
-    .yellow {
-        color: rgb(204, 175, 45);
-    }
-    .container {
-        font-size: 12px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        gap: 10px;
-        height: 100%;
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 100%;
-    }
+  .ok {
+    color: lightgreen;
+  }
+  .ko {
+    color: rgb(226, 88, 88);
+  }
 </style>
