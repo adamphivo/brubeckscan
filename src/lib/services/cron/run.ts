f79dbaca62cc94c610ff522e8156807b5382b41b
Adam Phi Vo <adamphivo@gmail.com>
2022-04-10 02:12:36 +0200

    Add persistent error logging to determine where cron runs bugs

--- src/lib/services/cron/run.ts
+++ src/lib/services/cron/run.ts
@@ -4,27 +4,42 @@ import StreamService from "$lib/services/stream";
 
 export async function run() {
     await StreamService.feedStream.publish("cronRun", "Run started 🤖");
-
     const start = Date.now();
-
-    const results = await Promise.all(tasks.map(task => task()));
-
-    const stop = Date.now();
+    try {
+        const results = await Promise.all(tasks.map(task => task()));
+        const stop = Date.now();
+        const runTimeInSeconds = (stop - start) / 1000;
     
-    const runTimeInSeconds = (stop - start) / 1000;
-
-    await prisma.cronRun.create({
-        data: {
-            finalStatus: 1,
-            runTimeInSeconds: runTimeInSeconds
-        }
-    });
+        await prisma.cronRun.create({
+            data: {
+                finalStatus: 1,
+                runTimeInSeconds: runTimeInSeconds
+            }
+        });
+        
+        await StreamService.feedStream.publish("cronRun", `CRON Run done in ${runTimeInSeconds} seconds 🔥`);
     
-    await StreamService.feedStream.publish("cronRun", `CRON Run done in ${runTimeInSeconds} seconds 🔥`);
+        return {
+            finalStatus: "OK",
+            cronRunTimeInSeconds: runTimeInSeconds,
+            results
+        };
+    } catch(e) {
+        const stop = Date.now();
+        const runTimeInSeconds = (stop - start) / 1000;
+        await prisma.cronRun.create({
+            data: {
+                finalStatus: 0,
+                runTimeInSeconds: runTimeInSeconds,
+                errorMessage: e.message
+            }
+        });
+        await StreamService.feedStream.publish("cronRun", `CRON Run errored in ${runTimeInSeconds} seconds 🐛`);
+        return {
+            finalStatus: "KO",
+            cronRunTimeInSeconds: runTimeInSeconds,
+            error: e
+        };
+    }
 
-    return {
-        finalStatus: "OK",
-        cronRunTimeInSeconds: runTimeInSeconds,
-        results
-    };
 }
